e9f93c8b95675515ae28f525f1af0806403f6a6b
Phương Ngô <maiyeuminhem_kothaydoi@outlook.com>
2020-06-24 15:59:41 +0700

    test

---
+++ leave-management/Data/LeaveAllocation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class LeaveAllocation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public IEnumerable<SelectListItem> Employees { get; set; }
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+}
